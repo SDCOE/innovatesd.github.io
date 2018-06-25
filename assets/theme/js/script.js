f8ffa6e1f109380ac5a1d8e9e8302f4887f76919
innovateSD <38679658+innovateSD@users.noreply.github.com>
2018-06-25 16:29:40 -0700

    create github pages

--- assets/theme/js/script.js
+++ assets/theme/js/script.js
@@ -1,5 +1,4 @@
-!function(){try{document.getElementsByClassName("engine")[0].getElementsByTagName("a")[0].removeAttribute("rel")}catch(b){}if(!document.getElementById("top-1")){var a=document.createElement("section");a.id="top-1";a.className="engine";a.innerHTML='<a href="https://mobiri.se">Mobirise</a> Mobirise v4.7.6';document.body.insertBefore(a,document.body.childNodes[0])}}();
-(function($) {
+undefined(function($) {
 
     var isBuilder = $('html').hasClass('is-builder');
 
